ef212952923b860c0def84a02d4c31f7b105f386
Jeremy Wagner <dev@musiquery.com>
2016-09-23 11:31:43 -0500

    Updated deploy script.

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,7 @@ git pull
 gulp clean
 gulp build
 rm -rf /var/www/caches/**
+chown -R apache:apache ./dist
+chown -R apache:apache ./caches
 /usr/local/apache2/bin/apachectl -k restart
 echo "Deployed."
