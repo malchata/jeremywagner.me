8f0dd53dac32560f76acaebda502c32d51cca804
Jeremy Wagner <jeremy.l.wagner@gmail.com>
2017-01-21 11:33:56 -0500

    Major stuff! - Added a new blog post. - Refactored some CSS and removed redundancies. - Changed little book thumbnail image to a CSS background image. It will now only load when the viewport is big enough to see it. - Updated NPM packages. - Ported gulp build system over to version 4. - Locked scrolling when the nav is open on mobile devices.

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,5 +1,6 @@
 (function(window, document){
 	document.addEventListener("DOMContentLoaded", function(){
+		bodyElement = document.getElementsByTagName("body")[0];
 		navIcon = document.getElementById("nav-icon");
 		navTrigger = document.getElementById("nav-trigger");
 		nav = document.getElementsByTagName("nav")[0];
@@ -13,6 +14,8 @@
 				navIcon.classList.add("opened");
 				nav.classList.add("open");
 				pageFade.classList.add("show");
+				bodyElement.style.overflowY = "hidden";
+				window.scrollTo(0, 0);
 			}
 			else{
 				closeNav();
@@ -23,6 +26,7 @@
 			navIcon.classList.remove("opened");
 			nav.classList.remove("open");
 			pageFade.classList.remove("show");
+			bodyElement.style.overflowY = "visible";
 		}
 	});
 })(window, document);
