bed2703ab3a99a1cb2c52f6c264b238747cde1f0
Jeremy Wagner <jeremy.l.wagner@gmail.com>
2017-01-21 17:09:04 -0500

    Tweaking deployment script.

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,35 @@
 clear
-echo "------------"
-echo "Deploying..."
-echo "------------"
+echo "-------------------"
+echo "Pulling from git..."
+echo "-------------------"
 git pull
+
+echo ""
+echo "Updating node modules..."
+echo "------------------------"
 npm update
+
+echo ""
+echo "----------------"
+echo "Building site..."
+echo "----------------"
 gulp build
+
+echo ""
+echo "---------------------------"
+echo "Clearing checksum caches..."
+echo "---------------------------"
 rm -rf /var/www/caches/**
 chown -R apache:apache ./dist
 chown -R apache:apache ./caches
+
+echo ""
+echo "---------------------------"
+echo "Restarting Apache server..."
+echo "---------------------------"
 /usr/local/apache2/bin/apachectl -k restart
+
+echo ""
 echo "---------"
-echo "Deployed."
+echo "Deployed!"
 echo "---------"
