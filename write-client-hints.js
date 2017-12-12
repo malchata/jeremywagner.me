44e6b90d53c379614825540733d6290f7929ebc8
Jeremy Wagner <jeremy.l.wagner@gmail.com>
2017-12-12 09:09:52 -0700

    Tweaked client hints a bit.

--- write-client-hints.js
+++ write-client-hints.js
@@ -1,35 +1,59 @@
 import assets from "./dist/assets-manifest.json";
 
-let hints = [assets["app.css"], assets["app.js"], assets["images/skyline.svg"]];
+let hints = [
+	assets["app.css"],
+	assets["app.js"],
+	assets["images/skyline.svg"],
+	assets["css/fonts/fredokaone.woff2"],
+	assets["css/fonts/monoton.woff2"]
+];
 let config = `<FilesMatch "\\.(html|html\\.gz|html\\.br)$">
 Header set Link "`;
 
 for(let asset in hints){
 	let assetType = "";
+	let assetContentType = "";
+	let assetParts = hints[asset].split(".");
 
-	switch(hints[asset].split(".")[hints.length - 1]){
+	switch(assetParts[assetParts.length - 1]){
 		case "css":
 			assetType = "style";
+			assetContentType = "text/css";
 		break;
 
 		case "js":
 			assetType = "script";
+			assetContentType = "text/javascript";
+		break;
+
+		case "jpg":
+		case "jpeg":
+			assetType = "image";
+			assetContentType = "image/jpeg";
+		break;
+
+		case "png":
+			assetType = "image";
+			assetContentType = "image/png";
+		break;
+
+		case "gif":
+			assetType = "image";
+			assetContentType = "image/gif";
 		break;
 
 		case "svg":
-		case "jpg":
-		case "jpeg":
-		case "png":
-		case "gif":
 			assetType = "image";
+			assetContentType = "image/svg+xml";
 		break;
 
 		case "woff2":
 			assetType = "font";
+			assetContentType = "font/woff2";
 		break;
 	}
 
-	config += `<${hints[asset]}>;rel=preload;as=${assetType};nopush`;
+	config += `<${hints[asset]}>; rel=preload; as=${assetType}; type=${assetContentType}; nopush`;
 
 	if(hints.indexOf(hints[asset]) < hints.length - 1){
 		config += ",";
